4068322d93bfceacdbae7404dbdd2d38efd97aaf
sampage10 <134566800+sampage10@users.noreply.github.com>
2025-05-07 01:56:25 -0400

    local data

--- src/workout/ExerciseLibrary.java
+++ src/workout/ExerciseLibrary.java
@@ -5,11 +5,11 @@ import java.util.*;
 public class ExerciseLibrary {
     public static List<Exercise> getPredefinedExercises() {
         List<Exercise> list = new ArrayList<>();
-        list.add(new Exercise("Squat", 3, 8, 5));
-        list.add(new Exercise("Deadlift", 3, 5, 5));
-        list.add(new Exercise("Bench Press", 3, 8, 5));
-        list.add(new Exercise("Barbell Row", 3, 12, 5));
-        list.add(new Exercise("Overhead Press", 3, 10, 5));
+        list.add(new Exercise("Squat", 3, 8, 60));
+        list.add(new Exercise("Deadlift", 3, 5, 90));
+        list.add(new Exercise("Bench Press", 3, 8, 60));
+        list.add(new Exercise("Barbell Row", 3, 12, 60));
+        list.add(new Exercise("Overhead Press", 3, 10, 80));
         return list;
     }
 }
